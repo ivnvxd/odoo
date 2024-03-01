f9f2915eabc6eeb09a7aa346fcea39a753b1f5f3
Andrey Ivanov <ivnv.xd@gmail.com>
2024-03-01 13:32:08 +0100

    Add best price computed field

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -52,8 +52,14 @@ class RealEstateProperty(models.Model):
     total_area = fields.Integer(
         compute="_compute_total_area", string="Total Area (sqm)"
     )
+    best_price = fields.Float(compute="_compute_best_price", string="Best Offer")
 
     @api.depends("living_area", "garden_area")
     def _compute_total_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
+
+    @api.depends("offer_ids.price")
+    def _compute_best_price(self):
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped("price"), default=0.0)
