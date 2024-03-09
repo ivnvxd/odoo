09282677a1c6e1dbec87eb32dbc107048018418a
Andrey Ivanov <ivnv.xd@gmail.com>
2024-03-09 13:37:25 +0100

    Add invoice creation on property sale

--- addons/estate_account/models/estate_property.py
+++ addons/estate_account/models/estate_property.py
@@ -1,11 +1,32 @@
-import logging
-
-from odoo import models
+from odoo import Command, models
 
 
 class EstateProperty(models.Model):
     _inherit = "estate.property"
 
     def action_sold(self):
-        logging.warning("EstateProperty action_sold called")
+        for record in self:
+            self.env["account.move"].create(
+                {
+                    "partner_id": record.buyer_id.id,
+                    "move_type": "out_invoice",
+                    "invoice_line_ids": [
+                        Command.create(
+                            {
+                                "name": "Property Sale",
+                                "quantity": 1,
+                                "price_unit": record.selling_price * 0.06,
+                            }
+                        ),
+                        Command.create(
+                            {
+                                "name": "Administrative Fees",
+                                "quantity": 1,
+                                "price_unit": 100.00,
+                            }
+                        ),
+                    ],
+                }
+            )
+
         return super().action_sold()
