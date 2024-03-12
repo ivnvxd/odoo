9a0bc3c68c3283edfc1b7eeacfbb88496e0937e3
Andrey Ivanov <ivnv.xd@gmail.com>
2024-03-12 16:10:34 +0100

    Add Card component

--- addons/awesome_owl/static/src/playground.js
+++ addons/awesome_owl/static/src/playground.js
@@ -1,9 +1,16 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, markup } from "@odoo/owl";
 import { Counter } from "./counter/counter";
+import { Card } from "./card/card";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-    static components = { Counter };
+    static components = { Counter, Card };
+
+    setup() {
+        this.value1 = "<div class='text-primary'>some text 1</div>";
+        this.value2 = markup("<div class='text-primary'>some text 2</div>");
+    }
+
 }
