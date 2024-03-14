aff718d28b636e86ca727ffb4612d8217e77d917
Andrey Ivanov <ivnv.xd@gmail.com>
2024-03-14 13:20:41 +0100

    Add autofocus to input field in TodoList component

--- addons/awesome_owl/static/src/todo_list/todo_list.js
+++ addons/awesome_owl/static/src/todo_list/todo_list.js
@@ -2,6 +2,7 @@
 
 import { Component, useState } from "@odoo/owl";
 import { TodoItem } from "./todo_item";
+import { useAutofocus } from "../utils";
 
 export class TodoList extends Component {
     static template = "awesome_owl.TodoList";
@@ -10,6 +11,7 @@ export class TodoList extends Component {
     setup() {
         this.todos = useState([]);
         this.todoIdCounter = 0;
+        useAutofocus("input");
     }
 
     addTodo(ev) {
