9f5c6e37d4b84c7b178bd0efdcdc21679135f87e
Andrey Ivanov <ivnv.xd@gmail.com>
2024-03-14 12:40:22 +0100

    Add functionality to add new tasks in TodoList component

--- addons/awesome_owl/static/src/todo_list/todo_list.js
+++ addons/awesome_owl/static/src/todo_list/todo_list.js
@@ -8,9 +8,18 @@ export class TodoList extends Component {
     static components = { TodoItem };
 
     setup() {
-        this.todos = useState([
-            { id: 2, description: "write tutorial", isCompleted: true },
-            { id: 3, description: "buy milk", isCompleted: false },
-        ]);
+        this.todos = useState([]);
+        this.todoIdCounter = 0;
+    }
+
+    addTodo(ev) {
+        if (ev.keyCode === 13 && ev.target.value !== "") {
+            this.todos.push({
+                id: this.todoIdCounter++,
+                description: ev.target.value,
+                isCompleted: false,
+            });
+            ev.target.value = "";
+        }
     }
 }
